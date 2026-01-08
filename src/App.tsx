280bf86a2985980c7fb1838b8a5ac48dda5f194d
Mluleki <melokuhlemluleki02@gmail.com>
2026-01-08 21:27:34 +0200

    Removed the Home text link from the navigation corner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <nav className="p-4 flex gap-4 border-b mb-4">
-        <Link to="/">Home</Link>
-        {/* <Link to="/history">History</Link> */}
+        {/* Navigation items can be added here */}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
